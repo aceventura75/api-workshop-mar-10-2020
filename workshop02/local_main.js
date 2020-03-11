1f9a4b6a29dc017b8a3259f5c9296b5f07eff658
unknown <nariyer@gmail.com>
2020-03-11 17:29:39 +0800

    workshop02

--- workshop02/local_main.js
+++ workshop02/local_main.js
@@ -18,15 +18,63 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mandatory workshop
 // TODO GET /api/states
+app.get('/api/states',
+	(req, resp) => { //handler
+		const result = db.findAllStates()
+		//status code
+		resp.status(200)
+		//set content-type
+		resp.type('application/json')
+		resp.set('X-genereated-on', (new Date()).toDateString())
+		resp.json(result)
 
+	}
+)
 
 // TODO GET /api/state/:state
+app.get('/api/states/:state',
+	(req, resp) => { //handler
+		//Read the value from  the route:state
+		const state = req.params.state
+		//Read the query string
+		const limit = parseInt(req.query.limit) || 10;
+		const offset = parseInt(req.query.offset) || 0;
 
+		//10 result from the top
+		const result = db.findCitiesByState(state,
+			{ offset: offset, limit: limit })
+		//status code
+		resp.status(200)
+		//set content-type
+		resp.type('application/json')
+		resp.json(result)
 
+	}
+)
+//TODO DELETE /api/city/:name
 // TODO GET /api/city/:cityId
 
-
 // TODO POST /api/city
+//Content-type:application/x-www-from-urlencoded
+app.post('/api/city',
+	(req, resp) => {
+		const boday = req.body;
+		console.info('body', body)
+		if (!db.validateForm(body)) {
+			resp.status(400)
+			resp.type('application/json')
+			resp.json({ 'message': 'incomplete form' })
+			return
+		}
+
+		//insert db
+		//TODO loc = "number,number" => [ number, number ]
+		db.insertCity(body)
+		resp.status(201)
+		resp.type('application/json')
+		resp.json({ 'message': 'created' })
+	}
+)
 
 // Optional workshop
 // TODO HEAD /api/state/:state
@@ -35,7 +83,24 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // TODO GET /state/:state/count
+app.get('/api/states/:state/count',
+	(req, resp) => { //handler
+		const state = req.params.state
+		const count = db.countCitiesInState(state)
+		const result = {
+			state: state,
+			numOfCities: count,
+			timestamp: (new Date()).toDateString()
+		}
 
+		//status code
+		resp.status(200)
+		//set content-type
+		resp.type('application/json')
+		resp.json(result)
+
+	}
+)
 
 // TODO GET /api/city/:name
 
